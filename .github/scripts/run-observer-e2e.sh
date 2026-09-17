aec2e908bad318f33124143832bda210ea1e5533
chinper lin <chinper@gmail.com>
2026-09-18 01:27:46 +0800

    ci: run clients on dummy Xorg display

--- .github/scripts/run-observer-e2e.sh
+++ .github/scripts/run-observer-e2e.sh
@@ -123,7 +123,7 @@ fi
 
 (
   cd build/e2e/target
-  exec xvfb-run -a -s "-screen 0 1280x720x24 +extension GLX +render +iglx -noreset" "$java_bin" @"$target_args" \
+  bash "$GITHUB_WORKSPACE/.github/scripts/run-glx-client.sh" "$java_bin" @"$target_args" \
     -Dfabric.dli.config="$launch_cfg" \
     -Dfabric.dli.env=client \
     -Dfabric.dli.main=net.fabricmc.loader.impl.launch.knot.KnotClient \
@@ -141,7 +141,7 @@ target_pid=$!
 # Gradle/Loom preparation completed before the first client can join.
 (
   cd build/e2e/observer
-  exec xvfb-run -a -s "-screen 0 1280x720x24 +extension GLX +render +iglx -noreset" "$java_bin" @"$observer_args" \
+  bash "$GITHUB_WORKSPACE/.github/scripts/run-glx-client.sh" "$java_bin" @"$observer_args" \
     -Dfabric.dli.config="$launch_cfg" \
     -Dfabric.dli.env=client \
     -Dfabric.dli.main=net.fabricmc.loader.impl.launch.knot.KnotClient \
